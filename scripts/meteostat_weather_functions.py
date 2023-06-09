b1aae219bb76a9122d7eb401f4215bd803ce00f7
Josh Knight <76140279+jknitey@users.noreply.github.com>
2023-06-09 09:16:46 -0600

    Added examples notebook. Added time column to weather data

--- scripts/meteostat_weather_functions.py
+++ scripts/meteostat_weather_functions.py
@@ -15,17 +15,17 @@ def get_weather_data(id, type, latitude, longitude, start_date, end_date) -> pd.
         # Get hourly data
         final_weather_data = Hourly(place, start, end)
         final_weather_data = final_weather_data.fetch()
-        data_col = ['temp', 'dwpt', 'rhum', 'prcp', 'snow', 'wdir', 'wspd', 'wpgt', 'pres', 'tsun', 'coco']
+        data_col = ['time', 'temp', 'dwpt', 'rhum', 'prcp', 'snow', 'wdir', 'wspd', 'wpgt', 'pres', 'tsun', 'coco']
     elif type == 'daily':
         # Get daily data
         final_weather_data = Daily(place, start, end)
         final_weather_data = final_weather_data.fetch()
-        data_col = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'wdir', 'wspd', 'wpgt', 'pres', 'tsun']
+        data_col = ['time', 'tavg', 'tmin', 'tmax', 'prcp', 'snow', 'wdir', 'wspd', 'wpgt', 'pres', 'tsun']
     elif type == 'monthly':
         # Get daily data
         final_weather_data = Monthly(place, start, end)
         final_weather_data = final_weather_data.fetch()
-        data_col = ['tavg', 'tmin', 'tmax', 'prcp', 'wspd', 'pres', 'tsun']
+        data_col = ['time', 'tavg', 'tmin', 'tmax', 'prcp', 'wspd', 'pres', 'tsun']
     else:
         print('Invalid type, please use hourly, daily, or monthly for input')
         
